04558f095a855ccee9291376094bbaa43feb08e5
Nelson Chen <nelsonchen5@gmail.com>
2019-02-05 14:46:05 -0800

    trello js put callback into promise

--- src/functions/trello.js
+++ src/functions/trello.js
@@ -7,10 +7,10 @@ exports.handler = function(event, context, callback) {
       `https://api.trello.com/1/cards/5c51f4e868d8af5615ce8cc9?fields=all&attachments=false&attachment_fields=all&members=false&membersVoted=false&checkItemStates=false&checklists=none&checklist_fields=all&board=false&list=false&pluginData=false&stickers=false&sticker_fields=all&customFieldItems=false&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`
     )
     .then(res => {
-      console.log(res)
+     
+      callback(null, {
+        statusCode: 200,
+        body: res,
+      })
     })
-  callback(null, {
-    statusCode: 200,
-    body: 'Trello pull works',
-  })
 }
