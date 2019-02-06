4e8aac8364d5d139192a37173eb6af3b1a8275dc
Nelson Chen <nelsonchen5@gmail.com>
2019-02-05 16:58:49 -0800

    trello

--- src/functions/trello.js
+++ src/functions/trello.js
@@ -6,10 +6,10 @@ exports.handler = function(event, context, callback) {
     .get(
       `https://api.trello.com/1/cards/5c51f4e868d8af5615ce8cc9?fields=all&attachments=false&attachment_fields=all&members=false&membersVoted=false&checkItemStates=false&checklists=none&checklist_fields=all&board=false&list=false&pluginData=false&stickers=false&sticker_fields=all&customFieldItems=false&key=${process.env.TRELLO_KEY}&token=${process.env.TRELLO_TOKEN}`
     )
-    .then(res => { 
+    .then( (res) => { 
       callback(null, {
         statusCode: 200,
-        body: res,
+        body: {msg:'nothing'},
       })
     })
 }
