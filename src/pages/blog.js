d845e427f10807fb32f62c676a859fcf3bc5a72e
Nelson Chen <nelsonchen5@gmail.com>
2019-02-05 21:33:21 -0800

    list/board logic

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -15,25 +15,22 @@ class SecondPage extends React.Component {
 
   componentDidMount() {
     axios.get('/.netlify/functions/trello').then((res)=>{
-      // this.setState({trelloData:res})
-      console.log(res)
+      const sortedData = res.data.sort((a, b)=>{
+        return new Date(b.date) - new Date(a.date);
+      })
+      this.setState({trelloData:sortedData})
     })
   }
 
-  combineList = () =>{
-    //go through every "List", and combine the post into one list
-    let post = []
-    this.state.trelloData.lists.forEach((item)=>{
-      
-    })
-  }
   render(){
   return(
   <Layout>
     <h1>Blog</h1>
     <p>Welcome to my blog</p>
   {/*make a list of all cards sorted by date*/}
-  
+  {this.state.trelloData.map((item)=>(
+    <h1> {item}</h1>
+  ))}
   {/*make all cards clickable*/}
   {/*onClick, make it link to a page with just blog post and comments*/}
     <Link to="/">Go back to the homepage</Link>
