ce73cdbb5e5a49e5962780883489961b1ebf91a6
Nelson Chen <nelsonchen5@gmail.com>
2019-02-05 16:40:11 -0800

    trello api key/token

--- src/functions/trello.js
+++ src/functions/trello.js
@@ -4,7 +4,7 @@ exports.handler = function(event, context, callback) {
   // your server-side functionality
   axios
     .get(
-      `https://api.trello.com/1/cards/5c51f4e868d8af5615ce8cc9?fields=all&attachments=false&attachment_fields=all&members=false&membersVoted=false&checkItemStates=false&checklists=none&checklist_fields=all&board=false&list=false&pluginData=false&stickers=false&sticker_fields=all&customFieldItems=false&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`
+      `https://api.trello.com/1/cards/5c51f4e868d8af5615ce8cc9?fields=all&attachments=false&attachment_fields=all&members=false&membersVoted=false&checkItemStates=false&checklists=none&checklist_fields=all&board=false&list=false&pluginData=false&stickers=false&sticker_fields=all&customFieldItems=false&key=${process.env.TRELLO_KEY}&token=${process.env.TRELLO_TOKEN}`
     )
     .then(res => { 
       callback(null, {
