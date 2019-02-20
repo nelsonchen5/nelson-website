181878771a6744e2cfa3c1dc0fd9debdac3a31fd
Nelson Chen <nelsonchen5@gmail.com>
2019-02-20 14:12:53 -0800

    blogpost

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -19,7 +19,7 @@ class SecondPage extends React.Component {
   async componentDidMount() {
     await axios.get('/.netlify/functions/trello').then(res => {
         console.log(res)
-        this.setState({ trelloData: res.data })
+        this.setState({ trelloData: dummyBlogs })
         this.sortData()   
     })
   }
@@ -35,6 +35,7 @@ class SecondPage extends React.Component {
   
     this.setState({sortedData: published})
   }
+
 
   render() {
     return (
